6ba9f0dd370c2f9387c3043e3eda831210021fc7
Matteo <spartacus990@gmail.com>
2022-09-06 13:40:18 +0200

    add grid search cv plotting function

--- Zillow/visualization.py
+++ Zillow/visualization.py
@@ -1,6 +1,9 @@
 from typing import Tuple
 from mpl_toolkits.basemap import Basemap
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import numbers
 
 from Zillow.data import property_land_use_type
 from Zillow.types import MoreFeatures as mft
@@ -28,3 +31,83 @@ class ZillowMap(Basemap):
         self.fillcontinents(color='#f6fbd7',lake_color='#d8eaf3')
         self.drawcoastlines(color='gray')
         self.drawcounties(color='darkgrey', linewidth=.3)
+
+
+def plot_grid_search_validation_curve(grid, param_to_vary,
+                                      title='Validation Curve', ylim=None,
+                                      xlim=None, log=None):
+    """Plots train and cross-validation scores from a GridSearchCV instance's
+    best params while varying one of those params."""
+
+    df_cv_results = pd.DataFrame(grid.cv_results_)
+    train_scores_mean = df_cv_results['mean_train_score']
+    valid_scores_mean = df_cv_results['mean_test_score']
+    train_scores_std = df_cv_results['std_train_score']
+    valid_scores_std = df_cv_results['std_test_score']
+
+    param_cols = [c for c in df_cv_results.columns if c[:6] == 'param_']
+    param_ranges = [grid.param_grid[p[6:]] for p in param_cols]
+    param_ranges_lengths = [len(pr) for pr in param_ranges]
+
+    train_scores_mean = np.array(train_scores_mean).reshape(*param_ranges_lengths)
+    valid_scores_mean = np.array(valid_scores_mean).reshape(*param_ranges_lengths)
+    train_scores_std = np.array(train_scores_std).reshape(*param_ranges_lengths)
+    valid_scores_std = np.array(valid_scores_std).reshape(*param_ranges_lengths)
+
+    param_to_vary_idx = param_cols.index('param_{}'.format(param_to_vary))
+
+    slices = []
+    for idx, param in enumerate(grid.best_params_):
+        if (idx == param_to_vary_idx):
+            slices.append(slice(None))
+            continue
+        best_param_val = grid.best_params_[param]
+        idx_of_best_param = 0
+        if isinstance(param_ranges[idx], np.ndarray):
+            idx_of_best_param = param_ranges[idx].tolist().index(best_param_val)
+        else:
+            idx_of_best_param = param_ranges[idx].index(best_param_val)
+        slices.append(idx_of_best_param)
+
+    train_scores_mean = train_scores_mean[tuple(slices)]
+    valid_scores_mean = valid_scores_mean[tuple(slices)]
+    train_scores_std = train_scores_std[tuple(slices)]
+    valid_scores_std = valid_scores_std[tuple(slices)]
+
+    plt.clf()
+
+    plt.title(title)
+    plt.xlabel(param_to_vary)
+    plt.ylabel('Score')
+
+    if (ylim is None):
+        plt.ylim(0.0, 1.1)
+    else:
+        plt.ylim(*ylim)
+
+    if (not (xlim is None)):
+        plt.xlim(*xlim)
+
+    lw = 2
+
+    plot_fn = plt.plot
+    if log:
+        plot_fn = plt.semilogx
+
+    param_range = param_ranges[param_to_vary_idx]
+    if (not isinstance(param_range[0], numbers.Number)):
+        param_range = [str(x) for x in param_range]
+    plot_fn(param_range, train_scores_mean, label='Training score', color='r',
+            lw=lw)
+    plt.fill_between(param_range, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color='r', lw=lw)
+    plot_fn(param_range, valid_scores_mean, label='Cross-validation score',
+            color='b', lw=lw)
+    plt.fill_between(param_range, valid_scores_mean - valid_scores_std,
+                     valid_scores_mean + valid_scores_std, alpha=0.1,
+                     color='b', lw=lw)
+
+    plt.legend(loc='lower right')
+
+    plt.show()
